a23b49839bf81e729706ae22dbc5aaeed950c633
rayud <rayudu21@gmail.com>
2023-11-30 21:20:00 +0530

    updated docker push

--- trivy-docker-image-scan.sh
+++ trivy-docker-image-scan.sh
@@ -5,6 +5,10 @@
 dockerImageName=$(awk 'NR==1 {print $2}' Dockerfile)
 echo $dockerImageName
 
+EXPORT WORKSPACE=/home/ubuntu
+
+echo $WORKSPACE
+
 docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 0 --severity HIGH --light $dockerImageName
 docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 1 --severity CRITICAL --light $dockerImageName
 
